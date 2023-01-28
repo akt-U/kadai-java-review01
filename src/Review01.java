6e5b062f81eadc8cb0dd8204ba09b3a0da53d20c
akt-U <welderakt@gmail.com>
2023-01-28 19:18:11 +0900

    int double 見直し

--- src/Review01.java
+++ src/Review01.java
@@ -4,14 +4,14 @@ public class Review01 {
     public static void main(String[] args) {
         int price = 1500;
         double zeiritu = 0.1;
-        double result;
-        result = taxmethod(price, zeiritu);
+        int result;
+        result = tax(price, zeiritu);
         double total = price + result;
-        System.out.println(price + "円の商品の税込み価格は" + Math.floor(total) + "円（消費税は" + Math.floor(result) + "円）です。 ");
+        System.out.println(price + "円の商品の税込価格は" + Math.floor(total) + "円（消費税は" + Math.floor(result) + "円）です。 ");
 
     }
-    public static double taxmethod(double price, double zeiritu) {
-        double result = price * zeiritu;
+    public static int tax(int price, double zeiritu) {
+        int result = (int) (price * zeiritu);
         return result;
     }
 
