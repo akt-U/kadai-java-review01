8da9d37769003b6daea55f85ce6de78444802120
akt-U <welderakt@gmail.com>
2023-01-28 19:20:58 +0900

    マスフロア削除

--- src/Review01.java
+++ src/Review01.java
@@ -6,8 +6,8 @@ public class Review01 {
         double zeiritu = 0.1;
         int result;
         result = tax(price, zeiritu);
-        double total = price + result;
-        System.out.println(price + "円の商品の税込価格は" + Math.floor(total) + "円（消費税は" + Math.floor(result) + "円）です。 ");
+        int total = price + result;
+        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + result + "円）です。 ");
 
     }
     public static int tax(int price, double zeiritu) {
